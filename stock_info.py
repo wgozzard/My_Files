ffbfa94d7e5ea9d26075d559aadc4243d4a62d3d
Will <72624648+wgozzard@users.noreply.github.com>
2021-03-03 00:24:51 -0500

    Update stock_info.py
    This code was taken from my IBM Data Science Course Python Project

--- stock_info.py
+++ stock_info.py
@@ -10,3 +10,66 @@ import requests
 from bs4 import BeautifulSoup
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+
+#Create interactive Graph function
+def make_graph(stock_data, revenue_data, stock):
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, subplot_titles=("Historical Share Price", "Historical Revenue"), vertical_spacing = .3)
+    fig.add_trace(go.Scatter(x=pd.to_datetime(stock_data.Date, infer_datetime_format=True), y=stock_data.Close.astype("float"), name="Share Price"), row=1, col=1)
+    fig.add_trace(go.Scatter(x=pd.to_datetime(revenue_data.Date, infer_datetime_format=True), y=revenue_data.Revenue.astype("float"), name="Revenue"), row=2, col=1)
+    fig.update_xaxes(title_text="Date", row=1, col=1)
+    fig.update_xaxes(title_text="Date", row=2, col=1)
+    fig.update_yaxes(title_text="Price ($US)", row=1, col=1)
+    fig.update_yaxes(title_text="Revenue ($US Millions)", row=2, col=1)
+    fig.update_layout(showlegend=False,
+    height=900,
+    title=stock,
+    xaxis_rangeslider_visible=True)
+    fig.show()
+    
+#Question 1: Use yfinance to Extract Stock Data
+#Using the Ticker function enter the ticker symbol of the stock we want to extract data on to create a ticker object. The stock is Tesla and its ticker symbol is TSLA.
+
+tesla_stonk = yf.Ticker("TSLA")
+#Using the ticker object and the function history extract stock information and save it in a dataframe named tesla_data. Set the period parameter to max so we get information for the maximum amount of time.
+
+ts = tesla_stonk.history(period = 'max')
+tesla_data = pd.DataFrame(ts)
+
+#Reset the index using the reset_index(inplace=True) function on the tesla_data DataFrame and display the first five rows of the tesla_data dataframe using the head function. Take a screenshot of the results and code from the beginning of Question 1 to the results below.
+
+tesla_data.reset_index(inplace=True)
+tesla_data.head()
+
+#Question 2: Use Webscraping to Extract Tesla Revenue Data
+#Use the requests library to download the webpage https://www.macrotrends.net/stocks/charts/TSLA/tesla/revenue. Save the text of the response as a variable named html_data.
+
+url = 'https://www.macrotrends.net/stocks/charts/TSLA/tesla/revenue'
+html_data = requests.get(url).text
+
+#Parse the html data using beautiful_soup.
+soup = BeautifulSoup(html_data, 'html5lib')
+
+#Using beautiful soup extract the table with Tesla Quarterly Revenue and store it into a dataframe named tesla_revenue. The dataframe should have columns Date and Revenue. Make sure the comma and dollar sign is removed from the Revenue column.
+
+#Built this scraper to take scrape stock data from the above URL which eventually is used to graph historical stock price vs revenue
+tables = soup.find_all('table')
+for index,table in enumerate(tables):
+    if ("Tesla Quarterly Revenue" in str(table)):
+        table_index = index
+tesla_revenue = pd.DataFrame(columns=["Date", "Revenue"])
+​
+for row in tables[table_index].tbody.find_all("tr"):
+    col = row.find_all("td")
+    if (col != []):
+        Date = col[0].text.strip()
+        Revenue = col[1].text.strip()
+        Revenue = Revenue.replace(',','')
+        Revenue = Revenue.replace('$','')
+        if Date and Revenue != '':
+            tesla_revenue = tesla_revenue.append({"Date":Date, "Revenue":Revenue}, ignore_index=True)
+tesla_revenue
+
+#Display the last 5 row of the tesla_revenue dataframe using the tail function. Take a screenshot of the results.
+
+#Had to remove null values for graph to work below
+tesla_revenue.tail()
