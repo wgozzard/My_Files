73b230f1de9906831df46ec06b77d860bd70a692
Will <72624648+wgozzard@users.noreply.github.com>
2021-03-03 00:15:15 -0500

    Create stock_info.py

---
+++ stock_info.py
@@ -0,0 +1,12 @@
+!pip install yfinance
+#!pip install pandas
+#!pip install requests
+!pip install bs4
+#!pip install plotly
+
+import yfinance as yf
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
