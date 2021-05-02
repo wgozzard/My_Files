750a3e20628beb8719be355c73334affefa0200c
Will <72624648+wgozzard@users.noreply.github.com>
2021-05-02 16:09:16 -0400

    Create parser_project.py

---
+++ parser_project.py
@@ -0,0 +1,3 @@
+#I built this for work. We had a very large spreadsheet to navigate on how offers would be distributed via guest concern. 
+#I read the spreadsheet into a dataframe so that we could parse the spreadsheet only finding rows related to our search.
+
