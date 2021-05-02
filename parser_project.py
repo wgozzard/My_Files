a07c9253b672f81429309055b27b9d32ede9bc70
Will <72624648+wgozzard@users.noreply.github.com>
2021-05-02 16:33:19 -0400

    Update parser_project.py
    I built this for my colleagues. We had a very large spreadsheet to navigate and this cut downtime effectively.  Essentially, if someone had an inquiry, for example, a problem with the app, I could type 'app' and the data frame will return all rows and columns associated with 'app' (not case senstative). If you plan to use this, you may need to tag a column with whatever you plan to search for.  For example, we had app - not working, app - enhancement, etc... each of these has a different value in the column. Create a new column and label each row in the new column app by app - not working for example and the spreadsheet will pull everything associated with that tag.

--- parser_project.py
+++ parser_project.py
@@ -1,3 +1,39 @@
 #I built this for work. We had a very large spreadsheet to navigate on how offers would be distributed via guest concern. 
 #I read the spreadsheet into a dataframe so that we could parse the spreadsheet only finding rows related to our search.
 
+#STEP ONE: PLEASE MAKE SURE 'spreadsheet' is loaded
+#PLEASE REMEMBER TO PRESS <------ PLAY BUTTON -- using google co-lab
+import pandas as pd
+def enter_category(cat_colab):
+  
+  file = 'enter spread sheet file here' 
+  df = pd.read_excel(file, sheet_name='spread sheet to read')
+  df1 = df.dropna() #drop null values
+  
+  if cat_colab == 'Oos' or cat_colab=='oos' or cat_colab =='OOS':
+    cat_colab = cat_colab.upper()
+    df_search = df1[(df1['Colab'] == cat_colab)]
+    return df_search
+  else:
+    x = cat_colab.title()
+    df_search = df1[(df1['Colab'] == x)]
+    return df_search
+#file variable above may skip the trial block since the function isn't called
+  try:
+  with open("FILE NAME", "rb") as f:
+    print("You are ready to search!")
+except:
+  print("PLEASE LOAD What Ever File that's loaded")
+  
+#This code will list the key search words that you can use above
+file = 'enter spread sheet file here' 
+df = pd.read_excel(file, sheet_name='spread sheet to read')
+df1 = df.dropna()
+num = 0 
+l_nm = []
+for i in df1['Colab']:
+  if i not in l_nm:
+    l_nm.append(i)
+for x in l_nm:
+  num += 1
+  print(num, x)
